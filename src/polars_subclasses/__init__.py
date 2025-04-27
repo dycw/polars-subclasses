81ab334e6a4008ab13f49bebf8f32031461af5a6
Derek Wan <d.wan@icloud.com>
2025-04-27 17:38:09 +0900

    Saved at 2025-04-27 17:38:09 (Sun)

--- src/polars_subclasses/__init__.py
+++ src/polars_subclasses/__init__.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from polars_subclasses.lib import DataFrameWithMetaData
 
-__version__ = "0.1.14"
+__version__ = "0.1.15"
 
 
 __all__ = ["DataFrameWithMetaData"]
