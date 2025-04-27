77a14e183f47bf8bde98de24d2cd6d257c364d3d
Derek Wan <d.wan@icloud.com>
2025-04-27 17:10:36 +0900

    Saved at 2025-04-27 17:10:36 (Sun)

--- src/tests/test_lib.py
+++ src/tests/test_lib.py
@@ -30,34 +30,41 @@ class TestDataFrameWithMetaData:
     @given(df=dataframes_with_bool())
     def test_explode(self, *, df: DataFrameWithBool) -> None:
         result = df.group_by("x").agg(col("x").alias("xs")).explode("xs")
+        assert isinstance(result, DataFrameWithBool)
         assert result.metadata is df.metadata
 
     @given(df=dataframes_with_bool())
     def test_filter(self, *, df: DataFrameWithBool) -> None:
         result = df.filter()
+        assert isinstance(result, DataFrameWithBool)
         assert result.metadata is df.metadata
 
     @given(df1=dataframes_with_bool(), df2=dataframes_with_bool())
     def test_join(self, *, df1: DataFrameWithBool, df2: DataFrameWithBool) -> None:
         result = df1.join(df2, on=["x"])
+        assert isinstance(result, DataFrameWithBool)
         assert result.metadata is df1.metadata
 
     @given(df=dataframes_with_bool())
     def test_rename(self, *, df: DataFrameWithBool) -> None:
         result = df.rename({"x": "x"})
+        assert isinstance(result, DataFrameWithBool)
         assert result.metadata is df.metadata
 
     @given(df=dataframes_with_bool())
     def test_select(self, *, df: DataFrameWithBool) -> None:
         result = df.select()
+        assert isinstance(result, DataFrameWithBool)
         assert result.metadata is df.metadata
 
     @given(df=dataframes_with_bool())
     def test_with_columns(self, *, df: DataFrameWithBool) -> None:
         result = df.with_columns()
+        assert isinstance(result, DataFrameWithBool)
         assert result.metadata is df.metadata
 
     @given(df=dataframes_with_bool())
     def test_with_row_index(self, *, df: DataFrameWithBool) -> None:
         result = df.with_row_index()
+        assert isinstance(result, DataFrameWithBool)
         assert result.metadata is df.metadata
