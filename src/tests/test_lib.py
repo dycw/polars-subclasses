addc50b6ac9a8fef5df7e0f84a7d18812a78eb17
Derek Wan <d.wan@icloud.com>
2025-04-27 17:25:14 +0900

    Saved at 2025-04-27 17:25:14 (Sun)

--- src/tests/test_lib.py
+++ src/tests/test_lib.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from hypothesis import given
+from hypothesis import assume, given
 from hypothesis.strategies import DrawFn, booleans, composite, lists, tuples
 from polars import DataFrame, Int64, String, col
 from utilities.hypothesis import int64s, text_ascii
@@ -65,6 +65,7 @@ class TestDataFrameWithMetaData:
 
     @given(df=dataframes_with_bool())
     def test_sample(self, *, df: DataFrameWithBool) -> None:
+        _ = assume(not df.is_empty())
         self._assert(df.sample(), df)
 
     @given(df=dataframes_with_bool())
