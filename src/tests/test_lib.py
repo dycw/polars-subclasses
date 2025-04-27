ccb4c6b392213f47591a1fc0f0fa91f2fb2f78f4
Derek Wan <d.wan@icloud.com>
2025-04-27 17:25:14 +0900

    Saved at 2025-04-27 17:25:14 (Sun)

--- src/tests/test_lib.py
+++ src/tests/test_lib.py
@@ -65,6 +65,7 @@ class TestDataFrameWithMetaData:
 
     @given(df=dataframes_with_bool())
     def test_sample(self, *, df: DataFrameWithBool) -> None:
+        _ = assume(not df.is_empty())
         self._assert(df.sample(), df)
 
     @given(df=dataframes_with_bool())
