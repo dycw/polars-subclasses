8bc5c668675a5ae77f2542f11c5a8fb29cad6fce
Derek Wan <d.wan@icloud.com>
2025-04-27 17:11:56 +0900

    Add `group_by` and `explode` (#8)

--- src/polars_subclasses/lib.py
+++ src/polars_subclasses/lib.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 from typing import TYPE_CHECKING, Any, Generic, Self, TypeVar, override
 
 from polars import DataFrame, Expr
+from polars.dataframe.group_by import GroupBy
 from polars.datatypes import N_INFER_DEFAULT
 
 if TYPE_CHECKING:
@@ -10,19 +11,22 @@ if TYPE_CHECKING:
 
     from numpy import ndarray
     from polars._typing import (
-        FrameInitTypes,
-        IntoExpr,
-        IntoExprColumn,
-        JoinStrategy,
-        JoinValidation,
-        MaintainOrderJoin,
-        Orientation,
-        SchemaDefinition,
-        SchemaDict,
+        FrameInitTypes,  # pyright: ignore[reportPrivateImportUsage]
+        IntoExpr,  # pyright: ignore[reportPrivateImportUsage]
+        IntoExprColumn,  # pyright: ignore[reportPrivateImportUsage]
+        JoinStrategy,  # pyright: ignore[reportPrivateImportUsage]
+        JoinValidation,  # pyright: ignore[reportPrivateImportUsage]
+        MaintainOrderJoin,  # pyright: ignore[reportPrivateImportUsage]
+        Orientation,  # pyright: ignore[reportPrivateImportUsage]
+        SchemaDefinition,  # pyright: ignore[reportPrivateImportUsage]
+        SchemaDict,  # pyright: ignore[reportPrivateImportUsage]
     )
 
 
 _T = TypeVar("_T")
+_TDataFrameWithMetaData = TypeVar(
+    "_TDataFrameWithMetaData", bound="DataFrameWithMetaData"
+)
 
 
 class DataFrameWithMetaData(DataFrame, Generic[_T]):
@@ -51,6 +55,14 @@ class DataFrameWithMetaData(DataFrame, Generic[_T]):
         self.metadata = metadata
 
     @override
+    def explode(
+        self, columns: str | Expr | Sequence[str | Expr], *more_columns: str | Expr
+    ) -> Self:
+        return type(self)(
+            data=super().explode(columns, *more_columns), metadata=self.metadata
+        )
+
+    @override
     def filter(
         self,
         *predicates: (
@@ -64,6 +76,23 @@ class DataFrameWithMetaData(DataFrame, Generic[_T]):
     ) -> Self:
         return type(self)(
             data=super().filter(*predicates, **constraints), metadata=self.metadata
+        )
+
+    @override
+    def group_by(
+        self,
+        *by: IntoExpr | Iterable[IntoExpr],
+        maintain_order: bool = False,
+        **named_by: IntoExpr,
+    ) -> GroupByWithMetaData[Self, _T]:
+        group_by = super().group_by(*by, maintain_order=maintain_order, **named_by)
+        return GroupByWithMetaData(
+            group_by.df,
+            *group_by.by,
+            maintain_order=group_by.maintain_order,
+            _cls=type(self),
+            _metadata=self.metadata,
+            **group_by.named_by,
         )
 
     @override
@@ -126,6 +155,28 @@ class DataFrameWithMetaData(DataFrame, Generic[_T]):
         return type(self)(
             data=super().with_row_index(name, offset), metadata=self.metadata
         )
+
+
+class GroupByWithMetaData(GroupBy, Generic[_TDataFrameWithMetaData, _T]):
+    @override
+    def __init__(
+        self,
+        df: DataFrame,
+        *by: IntoExpr | Iterable[IntoExpr],
+        maintain_order: bool,
+        _cls: type[_TDataFrameWithMetaData],
+        _metadata: _T,
+        **named_by: IntoExpr,
+    ) -> None:
+        super().__init__(df, *by, maintain_order=maintain_order, **named_by)
+        self.cls = _cls
+        self.metadata = _metadata
+
+    @override
+    def agg(
+        self, *aggs: IntoExpr | Iterable[IntoExpr], **named_aggs: IntoExpr
+    ) -> _TDataFrameWithMetaData:
+        return self.cls(data=super().agg(*aggs, **named_aggs), metadata=self.metadata)
 
 
 __all__ = ["DataFrameWithMetaData"]
